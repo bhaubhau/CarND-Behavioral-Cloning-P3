2958d521494e7760aa95d3646686868a2d34db8f
Bhavik Kawli <bhavik.kawli@gmail.com>
2019-05-26 05:39:31 +0000

    added augmentation, multicam and cropping

--- model.py
+++ model.py
@@ -17,19 +17,42 @@ for line in lines:
     center_path="./data/IMG/" + filename
     image=cv2.imread(center_path)
     images.append(image)
+    
+    left_path=line[1]
+    filename=left_path.split("/")[-1]
+    left_path="./data/IMG/" + filename
+    image=cv2.imread(left_path)
+    images.append(image)
+    
+    right_path=line[2]
+    filename=right_path.split("/")[-1]
+    right_path="./data/IMG/" + filename
+    image=cv2.imread(right_path)
+    images.append(image)
+    
     measurement=float(line[3])
     measurements.append(measurement)
+    measurements.append(measurement+0.2)
+    measurements.append(measurement-0.2)
    
-X_train=np.array(images)
-y_train=np.array(measurements)
+augmented_images, augmented_measurements=[], []
+for image,measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image,1))
+    augmented_measurements.append(measurement*-1.0)
+
+X_train=np.array(augmented_images)
+y_train=np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers.core import Flatten, Dense, Lambda
-from keras.layers.convolutional import Conv2D
-from keras.layers.pooling import MaxPooling2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
 
 model=Sequential()
 model.add(Lambda(lambda x:(x/255.0)-0.5,input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Conv2D(6,5,5,activation="relu"))
 model.add(MaxPooling2D())
 model.add(Conv2D(6,5,5,activation="relu"))
